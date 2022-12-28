c98f28857e4d4b3f986fe83bc00d35bf4559090c
KakaThanglerdsumpan <dhiraththanglerdsumpan@gmail.com>
2022-12-28 15:32:04 +0700

    started schedule page

--- pages/schedule_page.dart
+++ pages/schedule_page.dart
@@ -5,8 +5,40 @@ class SchedulePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Profile Page"),
+    return Center(
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: 4,
+        itemBuilder: (context, i) {
+          return Column(
+            children: [
+              const SizedBox(height: 10.0),
+              SizedBox(
+                width: 390.0,
+                height: 48.0,
+                child: Row(
+                  children: [
+                    ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemCount: 5,
+                        itemBuilder: (context, j) {
+                          return Container(
+                            width: 390 / 5,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5.0))),
+                          );
+                        }),
+                  ],
+                ),
+              )
+            ],
+          );
+        },
+      ),
     );
   }
 }
